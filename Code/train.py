5ffb5d1cb0b88bbbd47b364d0c00a9dda019eb3f
Capoo Cat <62407841+StarxSky@users.noreply.github.com>
2023-07-13 17:45:07 +0800

    Update train.py

--- Code/train.py
+++ Code/train.py
@@ -85,15 +85,15 @@ def trainer(CFG=CFG) :
 if __name__ == '__main__' :
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("train_size", type = float, default=0.9, help="train / val Dataset random split rate")
-    parser.add_argument("wandb", type = bool, default=False, help="Wandb api (default is Flase, if you need please keep it on.)")
-    parser.add_argument("weight_decay", type = float, default=0.01, help="weight decay (float)")
-    parser.add_argument("num_workers", type = int, default=2)
-    parser.add_argument("max_grad_norm", type = float, default=100.0, help = "Grad clip")
-    parser.add_argument("n_epochs", type = int, default=100, help = "Model traing epochs")
-    parser.add_argument("batch_size", type = int, default=128, help = "Batch size for Protein feature dataset")
-    parser.add_argument("lr", type = float, default=1e-5, help = "Training learning rate (default : 1e-5)")
-    parser.add_argument("conv_input_dim", type = int, default= 512, help="Model input data dim") 
+    parser.add_argument("--train_size", type = float, default=0.9, help="train / val Dataset random split rate")
+    parser.add_argument("--wandb", type = bool, default=False, help="Wandb api (default is Flase, if you need please keep it on.)")
+    parser.add_argument("--weight_decay", type = float, default=0.01, help="weight decay (float)")
+    parser.add_argument("--num_workers", type = int, default=2)
+    parser.add_argument("--max_grad_norm", type = float, default=100.0, help = "Grad clip")
+    parser.add_argument("--n_epochs", type = int, default=100, help = "Model traing epochs")
+    parser.add_argument("--batch_size", type = int, default=128, help = "Batch size for Protein feature dataset")
+    parser.add_argument("--lr", type = float, default=1e-5, help = "Training learning rate (default : 1e-5)")
+    parser.add_argument("--conv_input_dim", type = int, default= 512, help="Model input data dim") 
 
     args = parser.parse_args()
     
